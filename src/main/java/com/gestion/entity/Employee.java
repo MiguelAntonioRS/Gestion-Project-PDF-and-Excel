dd9f5348f05eba78c86079a919b26f4a6717f338
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-02-06 12:53:11 -0500

    Adding validations in the entity Employee

--- src/main/java/com/gestion/entity/Employee.java
+++ src/main/java/com/gestion/entity/Employee.java
@@ -1,13 +1,15 @@
 package com.gestion.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,15 +22,27 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     private String name;
 
+    @NotEmpty
     private String lastName;
 
+    @NotEmpty
+    @Email
     private String email;
 
+    @NotNull
     private int phone;
 
+    @NotEmpty
     private String sex;
 
+    @NotNull
     private double salary;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
 }
