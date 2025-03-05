971984d912f0d7de5c1164221d8ca139416f8c07
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-03-04 23:29:14 -0500

    Creating the methods in the class EmployeeDAO.java

--- src/main/java/com/gestion/persistence/EmployeeDAO.java
+++ src/main/java/com/gestion/persistence/EmployeeDAO.java
@@ -1,4 +1,22 @@
 package com.gestion.persistence;
 
+import com.gestion.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public interface EmployeeDAO {
+
+    public List<Employee> findAll();
+
+    public Page<Employee> findAll(Pageable pageable);
+
+    public void save (Employee employee);
+
+    public Employee findById (Long id);
+
+    public void delete(Long id);
 }
