2abf4b55231d6168bf681f8216c7c9777c5b13ec
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-03-04 23:32:39 -0500

    Implementing methods in the class EmployeeDAOImpl.java

--- src/main/java/com/gestion/persistence/implement/EmployeeDAOImpl.java
+++ src/main/java/com/gestion/persistence/implement/EmployeeDAOImpl.java
@@ -1,4 +1,43 @@
 package com.gestion.persistence.implement;
 
-public class EmployeeDAOImpl {
+import com.gestion.entity.Employee;
+import com.gestion.persistence.EmployeeDAO;
+import com.gestion.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Page<Employee> findAll(Pageable pageable) {
+        return employeeRepository.findAll(pageable);
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee findById(Long id) {
+        return employeeRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void delete(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
