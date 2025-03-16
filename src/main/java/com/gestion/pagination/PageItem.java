0670e56a49d8a1d86b0cdd23260a7b5514715377
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-03-16 13:44:39 -0400

    Updating the class PageItem

--- src/main/java/com/gestion/pagination/PageItem.java
+++ src/main/java/com/gestion/pagination/PageItem.java
@@ -1,4 +1,17 @@
 package com.gestion.pagination;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class PageItem {
+
+    private int number;
+
+    private boolean actually;
 }
