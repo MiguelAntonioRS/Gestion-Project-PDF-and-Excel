cdea8efb5084447cc8dd70839bc512c1dc0936b3
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-03-16 15:25:58 -0400

    Updating the class PageRender.java

--- src/main/java/com/gestion/pagination/PageRender.java
+++ src/main/java/com/gestion/pagination/PageRender.java
@@ -1,8 +1,11 @@
 package com.gestion.pagination;
 
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
+import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
 public class PageRender<T>{
 
     private String url;
@@ -11,4 +14,39 @@ public class PageRender<T>{
     private int numberOfElementPage;
     private int actualPage;
     private List<PageItem> pages;
+
+    private PageRender(String url, Page<T> page) {
+        this.url = url;
+        this.page = page;
+        this.pages = new ArrayList<PageItem>();
+
+        numberOfElementPage = 5;
+        totalPage = page.getTotalPages();
+        actualPage = page.getNumber();
+
+        int from, to;
+
+        if (totalPage <= numberOfElementPage) {
+
+            from = 1;
+            to = totalPage;
+        } else {
+            if (actualPage <= numberOfElementPage/2) {
+
+                from = 1;
+                to = numberOfElementPage;
+            } else if (actualPage >= totalPage - numberOfElementPage/2) {
+
+                from = totalPage - numberOfElementPage + 1;
+                to = numberOfElementPage;
+            }
+            else {
+                from = actualPage - numberOfElementPage/2;
+                to = numberOfElementPage;
+            }
+        }
+        for (int i = 0; i < from; i++) {
+            pages.add(new PageItem(from + 1, actualPage == from + i));
+        }
+    }
 }
