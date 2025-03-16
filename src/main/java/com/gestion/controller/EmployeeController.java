473dc398302f982ab9de398406f5892efd55b69a
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-03-16 15:34:09 -0400

    Adding the class controller EmployeeController

---
+++ src/main/java/com/gestion/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.gestion.controller;
+
+import com.gestion.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+}
