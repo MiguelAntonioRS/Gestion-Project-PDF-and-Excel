3de807745bcfdacb1e7685f52113e8d8ab4f49bc
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-04-07 11:00:08 -0400

    updating the controller class EmployeeController.java

--- src/main/java/com/gestion/controller/EmployeeController.java
+++ src/main/java/com/gestion/controller/EmployeeController.java
@@ -2,7 +2,12 @@ package com.gestion.controller;
 
 import com.gestion.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EmployeeController {
@@ -10,4 +15,9 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @GetMapping({"/","/listar",""})
+    public String employeeList(@RequestParam(name = "page", defaultValue = 0) int page, Model model) {
+
+        Pageable pageRequest = PageRequest.of(page, 5);
+    }
 }
