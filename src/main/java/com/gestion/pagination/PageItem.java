5a19349551f2a0418b69169deb39bb2a447c47e8
MiguelAntonioRS <rojassucarinomiguelantonio@gmail.com>
2025-06-26 17:20:51 -0400

    More Code

--- src/main/java/com/gestion/pagination/PageItem.java
+++ src/main/java/com/gestion/pagination/PageItem.java
@@ -14,4 +14,28 @@ public class PageItem {
     private int number;
 
     private boolean actually;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isActually() {
+        return actually;
+    }
+
+    public void setActually(boolean actually) {
+        this.actually = actually;
+    }
+
+    public PageItem(int number, boolean actually) {
+        this.number = number;
+        this.actually = actually;
+    }
+
+    public PageItem() {
+    }
 }
