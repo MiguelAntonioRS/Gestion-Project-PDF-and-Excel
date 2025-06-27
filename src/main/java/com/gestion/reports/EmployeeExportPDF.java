3321426112b163a7dda5465dfc9fa3667babc140
MiguelAntonioRS <rojassucarinomiguelantonio@gmail.com>
2025-06-26 23:24:58 -0400

    More Code

--- src/main/java/com/gestion/reports/EmployeeExportPDF.java
+++ src/main/java/com/gestion/reports/EmployeeExportPDF.java
@@ -1,11 +1,15 @@
 package com.gestion.reports;
 
 import com.gestion.entity.Employee;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfTable;
+import com.lowagie.text.Font;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.awt.*;
 import java.util.List;
 
 @AllArgsConstructor
@@ -17,6 +21,12 @@ public class EmployeeExportPDF {
     private List<Employee> employeeList;
 
     private void writeHeaderTable(PdfTable table) {
+        PdfPCell cell = new PdfPCell();
 
+        cell.setBackgroundColor(Color.RED);
+        cell.setPadding(5);
+
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(Color.WHITE);
     }
 }
