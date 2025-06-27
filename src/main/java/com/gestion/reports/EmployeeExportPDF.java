0e4a3c46cc135ad0b67b185597a4d91c3b8b75f6
MiguelAntonioRS <rojassucarinomiguelantonio@gmail.com>
2025-06-26 23:17:55 -0400

    First method for export PDF

--- src/main/java/com/gestion/reports/EmployeeExportPDF.java
+++ src/main/java/com/gestion/reports/EmployeeExportPDF.java
@@ -1,4 +1,22 @@
 package com.gestion.reports;
 
+import com.gestion.entity.Employee;
+import com.lowagie.text.pdf.PdfTable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class EmployeeExportPDF {
+
+    private List<Employee> employeeList;
+
+    private void writeHeaderTable(PdfTable table) {
+
+    }
 }
