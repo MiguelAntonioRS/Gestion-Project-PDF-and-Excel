deee4242793d4791a0e03075808f3d69b06dbda9
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-08-18 12:48:34 -0400

    Created the variables and constructor

--- src/main/java/com/gestion/reports/EmployeeExportExcel.java
+++ src/main/java/com/gestion/reports/EmployeeExportExcel.java
@@ -1,4 +1,20 @@
 package com.gestion.reports;
 
+import com.gestion.entity.Employee;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.util.List;
+
 public class EmployeeExportExcel {
+
+    private XSSFWorkbook book;
+    private XSSFSheet paper;
+
+    private List<Employee> employeeList;
+
+    public EmployeeExportExcel(XSSFWorkbook book, XSSFSheet paper, List<Employee> employeeList) {
+        this.book = book;
+        this.paper = paper;
+        this.employeeList = employeeList;
+    }
 }
