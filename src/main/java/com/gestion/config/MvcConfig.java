f6b2793d48f86ac90f627349fc5f9f2d8ef08058
Miguel <rojassucarinomiguelantonio@gmail.com>
2025-09-20 12:44:42 -0400

    Creating a class and moving another one

--- src/main/java/com/gestion/config/MvcConfig.java
+++ src/main/java/com/gestion/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.gestion;
+package com.gestion.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
---
+++ src/main/java/com/gestion/config/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.gestion.config;
+
+public class WebSecurityConfig {
+}
